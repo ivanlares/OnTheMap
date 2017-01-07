eb3743afcf0f8e542832386a21190600e07c1754
ivan lares <ivanlares1@gmail.com>
2017-01-06 21:31:49 -0800

    chore: configured login view controller

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -11,10 +11,35 @@ import UIKit
 
 class LoginViewController: UIViewController{
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
+        setDelegates()
     }
     
+    // MARK: Target Action
+    
     override func viewWillAppear(_ animated: Bool) {
+    }
+    
+    @IBAction func didPressLogin(_ sender: UIButton) {
+    }
+    
+    // MARK: Helper
+    
+    func setDelegates(){
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
     }
     
 }
