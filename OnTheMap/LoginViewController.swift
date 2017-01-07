616933d543c18b1228b5c2c7a43f30e5e6b8ef07
ivan lares <ivanlares1@gmail.com>
2017-01-06 21:12:29 -0800

    chore: removed auto enable from login view text fields

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by ivan lares on 1/6/17.
+//  Copyright © 2017 ivan lares. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+    override func viewDidLoad() {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
+    
+}
