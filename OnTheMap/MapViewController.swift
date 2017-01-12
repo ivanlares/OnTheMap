38dfacce4e1405b192dedac14bf8819ea174d9f4
ivan lares <ivanlares1@gmail.com>
2017-01-11 19:43:52 -0800

    chore: added map view

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by ivan lares on 1/11/17.
+//  Copyright © 2017 ivan lares. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+}
