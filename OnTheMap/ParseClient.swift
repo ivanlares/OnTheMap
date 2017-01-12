8ba69e09c27c5b8e2009076c144a1e663d6ed560
ivan lares <ivanlares1@gmail.com>
2017-01-11 21:49:27 -0800

    chore: added parse client

---
+++ OnTheMap/ParseClient.swift
@@ -0,0 +1,16 @@
+//
+//  ParseClient.swift
+//  OnTheMap
+//
+//  Created by ivan lares on 1/11/17.
+//  Copyright © 2017 ivan lares. All rights reserved.
+//
+
+import Foundation
+
+class ParseClient{
+    
+    static let sharedInstance = ParseClient()
+    
+    private init() {}
+}
---
+++ OnTheMap/ParseClientConstants.swift
@@ -0,0 +1,18 @@
+//
+//  ParseClientConstants.swift
+//  OnTheMap
+//
+//  Created by ivan lares on 1/11/17.
+//  Copyright © 2017 ivan lares. All rights reserved.
+//
+
+import Foundation
+
+extension ParseClient{
+    
+    struct Constants{
+        static let applicationId = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let apiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    }
+    
+}
