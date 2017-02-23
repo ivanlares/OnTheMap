f6d9faf88251dcf26c96dbe30287341c5e51dfa2
ivan lares <ivanlares1@gmail.com>
2017-02-22 21:45:53 -0800

    chore: added custom login error type

---
+++ OnTheMap/ErrorTypes.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorHelper.swift
+//  OnTheMap
+//
+//  Created by ivan lares on 2/22/17.
+//  Copyright © 2017 ivan lares. All rights reserved.
+//
+
+import Foundation
+
+enum LoginError: Error {
+    case UnableToRetrieveUserData
+}
