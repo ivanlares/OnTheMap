4240454b2fe0c0f21ca1ad5fb7290a2eb605be68
ivan lares <ivanlares1@gmail.com>
2017-03-19 23:22:12 -0700

    fix: next button enabling in InputLocationView

--- OnTheMap/InputLocationViewController.swift
+++ OnTheMap/InputLocationViewController.swift
@@ -103,12 +103,21 @@ extension InputLocationViewController: UITextFieldDelegate{
         return false
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let text = textField.text, !text.isEmpty{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let currentText = textField.text as NSString? else {
+            return true
+        }
+        let newString = currentText.replacingCharacters(in: range, with: string)
+        
+        if newString.isEmpty{
+            print("newString is empty")
+            nextButton.isEnabled = false
+            nextButton.alpha = 0.5
+        } else {
+            print("newString is not empty")
             nextButton.isEnabled = true
             nextButton.alpha = 1
-            return
         }
-        nextButton.isEnabled = false
+        return true
     }
 }
